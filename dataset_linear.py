dac6676c1934bbc2fa127126232a92fa54708384
mincudini <m.incudini@reply.it>
2022-06-21 11:04:16 +0200

    fixed dataset test

--- dataset_linear.py
+++ dataset_linear.py
@@ -22,6 +22,22 @@ class LinearRegressionDataset:
         return np.dot(w, x) + np.random.uniform(low=-noise/2, high=noise/2)
 
 
+def generate_dataset_process(directory, d, epsilon, n_train, n_test, seed):
+    np.random.seed(seed)
+    gm_train = LinearRegressionDataset(n=n_train, d=d, epsilon=epsilon)
+    np.save(f"{directory}/train_X.npy", gm_train.X)
+    np.save(f"{directory}/train_y.npy", gm_train.y)
+    gm_test = LinearRegressionDataset(n=n_test, d=d, epsilon=epsilon)
+    np.save(f"{directory}/test_X.npy", gm_test.X)
+    np.save(f"{directory}/test_y.npy", gm_test.y)
+    json.dump({'d': d, 'epsilon': epsilon, 'n_train': n_train, 'n_test': n_test, 'seed': seed}, open(f"{directory}/specs.json", "w"))
+    plt.scatter(gm_train.X[:,0], gm_train.X[:,1], c=gm_train.y)
+    plt.savefig(f"{directory}/training_set.png")
+    plt.close('all')
+    plt.scatter(gm_test.X[:,0], gm_test.X[:,1], c=gm_test.y)
+    plt.savefig(f"{directory}/testing_set.png")
+    plt.close('all')
+
 @click.group()
 def main():
     pass
@@ -35,20 +51,19 @@ def main():
 @click.option('--n-test', type=int, required=True)
 @click.option('--seed', type=int, required=True)
 def generate_dataset(directory, d, epsilon, n_train, n_test, seed):
-    np.random.seed(seed)
-    gm_train = LinearRegressionDataset(n=n_train, d=d, epsilon=epsilon)
-    np.save(f"{directory}/train_X.npy", gm_train.X)
-    np.save(f"{directory}/train_y.npy", gm_train.y)
-    gm_test = LinearRegressionDataset(n=n_train, d=d, epsilon=epsilon)
-    np.save(f"{directory}/test_X.npy", gm_test.X)
-    np.save(f"{directory}/test_y.npy", gm_test.y)
-    json.dump({'d': d, 'epsilon': epsilon, 'n_train': n_train, 'n_test': n_test, 'seed': seed}, open(f"{directory}/specs.json", "w"))
-    plt.scatter(gm_train.X[:,0], gm_train.X[:,1], c=gm_train.y)
-    plt.savefig(f"{directory}/training_set.png")
-    plt.close('all')
-    plt.scatter(gm_test.X[:,0], gm_test.X[:,1], c=gm_test.y)
-    plt.savefig(f"{directory}/testing_set.png")
-    plt.close('all')
+    generate_dataset_process(directory, d, epsilon, n_train, n_test, seed)
+
+
+@main.command()
+@click.option('--directory', type=click.Path(), required=True)
+def regenerate_dataset(directory):
+    specs = json.load(open(f"{directory}/specs.json"))
+    d = int(specs['d'])
+    n_train = int(specs['n_train'])
+    n_test = int(specs['n_test'])
+    seed = int(specs['seed'])
+    epsilon = float(specs['epsilon'])
+    generate_dataset_process(directory, d, epsilon, n_train, n_test, seed)
 
 
 if __name__ == '__main__':
